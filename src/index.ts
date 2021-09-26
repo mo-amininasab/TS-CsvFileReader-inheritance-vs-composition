37bd51c34b08e082ae5b161fbd223f0cfd2ad128
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 23:14:22 +0330

    class CsvFileReader added

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+let manUnitedWins = 0;
 
 enum MatchResult {
   HomeWin = 'H',
   AwayWin = 'A',
   Draw = 'D',
-};
+}
 
-let manUnitedWins = 0;
-
-for (let match of matches) {
+for (let match of reader.data) {
   if (
     (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
     (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
@@ -23,5 +19,7 @@ for (let match of matches) {
     manUnitedWins++;
   }
 }
+
+// console.log(reader.data);
 
 console.log(`Man United won ${manUnitedWins} games.`);
