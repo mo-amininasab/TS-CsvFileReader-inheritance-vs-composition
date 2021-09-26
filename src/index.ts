06e7624c8ef0e3d8f4bd9be3f0dd18584a3a24a1
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-27 00:36:26 +0330

    done

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { CsvFileReader } from './CsvFileReader';
-import {MatchResult} from './MatchResult'
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-
-
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (
     (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
     (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
@@ -18,5 +18,4 @@ for (let match of reader.data) {
 }
 
 console.log(`Man United won ${manUnitedWins} games.`);
-console.log(reader.data[0]);
-
+// console.log(reader.data[0]);
