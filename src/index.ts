de53812f8ae5cb47a78a98a1e5b94622b91fb74a
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 22:47:41 +0330

    enum added. now our code is more clear thanks to enum

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,21 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+};
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
   if (
-    (match[1] === 'Man United' && match[5] === 'H') ||
-    (match[2] === 'Man United' && match[5] === 'A')
+    (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
   ) {
     manUnitedWins++;
   }
 }
 
 console.log(`Man United won ${manUnitedWins} games.`);
-
